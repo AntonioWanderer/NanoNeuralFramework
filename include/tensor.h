979a0af9d8a7fea321af78312a912f27a04b8148
AgeikovAV <ageikow@mail.ru>
2026-01-27 15:24:54 +0700

    feat(tensor header): define basic operations and functions

--- include/tensor.h
+++ include/tensor.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+// ======Define Datatypes======
+
 typedef enum {
     DTYPE_FLOAT32,
     DTYPE_INT32,
@@ -20,7 +22,37 @@ typedef struct {
     bool owns_data; // flag that tensor owns current data, not using outside data
 } Tensor;
 
+// ======Create and destroy======
 
+Tensor* tensor_create(const int* shape, int ndim, DataType dtype);
+Tensor* tensor_zeros(const int* shape, int ndim, DataType dtype);
+Tensor* tensor_ones(const int* shape, int ndim, DataType dtype);
+Tensor* tensor_from_data(void* data, const int* shape, int ndim, DataType dtype);
 
+void tensor_free(Tensor* tensor);
+
+// ======Info======
+
+size_t tensor_size(const Tensor* tesor);
+size_t dtype_size(DataType dtype);
+void tensor_print(Tensor* tensor);
+
+// ======Data access======
+
+float* tensor_get_data_float(const Tensor* tensor);
+void* tensor_get_item(const Tensor* tensor, const int* indexes);
+void tensor_set_item(Tensor* tensor, const int* indexes, void* value);
+
+// ======Math operations======
+
+Tensor* tensor_add(const Tensor* first, const Tensor* second);
+Tensor* tensor_mul(const Tensor* first, const Tensor* second);
+Tensor* tensor_matmul_naive(const Tensor* first, const Tensor* second);
+Tensor* tensor_relu(const Tensor* tensor);
+
+// ======Optimized operations======
+
+Tensor* tensor_matmul_simd(const Tensor* first, const Tensor* second);
+Tensor* tensor_conv2d_naive(const Tensor* input, const Tensor* kernel);
 
 #endif
